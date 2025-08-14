86ca0496ba687a6769cf4a720b4a20d664247968
Sam Zanca <samuele@zanca.dev>
2025-08-14 00:17:14 -0400

    now saving floor plans but also only saving 1Bed

--- scrape.ts
+++ scrape.ts
@@ -11,17 +11,20 @@ type AptUnit = {
     deleted: boolean;
     timestamp: string;
   }>;
+  sf: number;
 };
 
 type Data = {
   timestamp: number;
   units: Record<string, AptUnit>;
+  // floor_plans: Array<string>;
 };
 
 enum Headers {
   residence = 0,
   bedroom = 1,
   bathroom = 2,
+  floor_plan = 3,
   price = 4,
 }
 
@@ -36,7 +39,16 @@ type UnitSnapshot = {
   bedroom: string;
   bathroom: string;
   price: number;
+  sf: number;
 };
+
+async function getSquareFoot(page: Page) {
+  const text = await page.getByText(/\d+\s?SF/).textContent()!;
+
+  // Use regex to extract just the number part from the text
+  const numberMatch = text?.match(/\d+/);
+  return numberMatch ? +numberMatch[0] : 0;
+}
 
 async function scrapeLiveData(): Promise<Map<string, UnitSnapshot>> {
   let browser: Browser | undefined; // Declare browser variable outside try-block
@@ -51,20 +63,46 @@ async function scrapeLiveData(): Promise<Map<string, UnitSnapshot>> {
 
     await page.goto(urlToScrape);
 
+    const floor_plans = new Map<string, number>();
+
     const rows = await page.$$(".tableRow[data-residence]");
-    for (const row of rows) {
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i];
       const cells = await row.$$("div");
 
+      const bedroom = (await cells[Headers.bedroom].textContent())!;
+
+      // Skip for non 1 bed
+      if (bedroom !== "1 Bedroom") continue;
+
       const priceStr = await cells[Headers.price].textContent();
+      const residence = (await cells[Headers.residence].textContent()) ?? "";
+
+      const plan = residence.substring(2, 4);
+
+      if (!floor_plans.has(plan)) {
+        await cells[Headers.floor_plan].click();
+        await page.waitForTimeout(500);
+        await page.screenshot({
+          path: path.join(__dirname, "data", `${bedroom}-${plan}-.png`),
+        });
+
+        const sf = await getSquareFoot(page);
+        await page.getByRole("button", { name: "Back" }).click();
+
+        floor_plans.set(plan, sf);
+      }
 
       const unit: UnitSnapshot = {
-        residence: (await cells[Headers.residence].textContent()) ?? "",
-        bedroom: (await cells[Headers.bedroom].textContent()) ?? "",
+        residence,
+        bedroom,
         bathroom: (await cells[Headers.bathroom].textContent()) ?? "",
         price: priceStr ? getPrice(priceStr) : 0,
+        sf: floor_plans.get(plan) ?? 0,
       };
 
       data.set(unit.residence, unit);
+      console.log(`Unit ${i + 1}/${rows.length}:`);
     }
   } catch (error: unknown) {
     // Use unknown for caught errors
@@ -114,6 +152,7 @@ function saveChanges(
   for (const [residence, updatedUnit] of updatedData.entries()) {
     if (!existingData.units[residence]) {
       existingData.units[residence] = {
+        sf: updatedUnit.sf,
         bathroom: updatedUnit.bathroom,
         bedroom: updatedUnit.bedroom,
         residence: updatedUnit.residence,
